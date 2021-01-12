6a5da0c0602e7863bc5c30cd1d65193af762aadf
smassarsky <seth.massarsky@gmail.com>
2021-01-12 17:07:31 -0500

    commit tree 73011f53f20ac25687768adbd44325622ca72588

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    })
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(json => document.querySelector("body").innerHTML += json.id)
+    .catch(error => {
+      alert("Unauthorized Access")
+      document.querySelector("body").innerHTML += error.message
+    })
+}
